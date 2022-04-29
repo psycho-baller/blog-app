d40bf2133e276d70994fa8fd27a8a872265c533a
psycho-baller <Ramim66809@gmail.com>
2022-04-29 02:15:56 -0600

    unauthorized to see commnit message B-)

--- pages/flashlight.tsx
+++ pages/flashlight.tsx
@@ -2,7 +2,7 @@
 // https://github.com/sickdyd/react-flashlight-demo/blob/master/src/App.js
 import type { NextPage } from "next";
 import Head from "next/head";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { ReactFlashlight } from "react-flashlight";
 import flashStyle from "../styles/index.module.scss";
 import { Howl, Howler } from "howler";
@@ -20,13 +20,11 @@ import { Howl, Howler } from "howler";
 //   }
 // }
 
-const isWindowContext = typeof window !== "undefined";
 const Flashlight: NextPage = () => {
   // const { results = [] } = joke
   // console.log(joke);
   // console.log(results);
-  const x = isWindowContext && window.innerWidth / 2;
-  const y = isWindowContext && window.innerHeight / 2;
+
   function knockKnock() {
     const sound = new Howl({
       src: ["knock-knock.mp3"],
@@ -35,13 +33,13 @@ const Flashlight: NextPage = () => {
       sound.play();
     });
   }
-  function gotchaBitch(){
+  function gotchaBitch() {
     const sound = new Howl({
-      src: ['gotcha-bitch.mp3'],
+      src: ["gotcha-bitch.mp3"],
     });
-    sound.play()
+    sound.play();
   }
-  function horn(){
+  function horn() {
     var sound = new Howl({
       src: ["air-horn.mp3"],
     });
@@ -56,10 +54,19 @@ const Flashlight: NextPage = () => {
     //   console.log("Finished!");
     // });
   }
+
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [isOn, setIsOn] = useState(true);
+
   useEffect(() => {
+    const isWindowContext = typeof window !== "undefined";
+    const x: number = (isWindowContext && window.innerWidth / 2) as number;
+    const y: number = (isWindowContext && window.innerHeight / 2) as number;
+    // const point: number = { x: x, y: y };
+    setPosition({ x: x, y: y });
     knockKnock();
   }, []);
-    
+
   return (
     <div className={flashStyle.fullScreen}>
       <Head>
@@ -68,7 +75,7 @@ const Flashlight: NextPage = () => {
         <link rel="icon" href="/torch.svg" />
       </Head>
       <ReactFlashlight
-        initialPosition={React.useState({ x: x, y: y })[0]}
+        initialPosition={position}
         size={150}
         // enableMouse={false}
         // showCursor={true}
@@ -86,7 +93,11 @@ const Flashlight: NextPage = () => {
           <div className={flashStyle.res}>
             <h1 className={flashStyle.center}>ligma who?</h1>
           </div>
-          <div className={flashStyle.star} onMouseEnter={horn} onMouseLeave={gotchaBitch}>
+          <div
+            className={flashStyle.star}
+            onMouseEnter={horn}
+            onMouseLeave={gotchaBitch}
+          >
             <h1 className={flashStyle.center}>ligma balls</h1>
           </div>
         </div>
